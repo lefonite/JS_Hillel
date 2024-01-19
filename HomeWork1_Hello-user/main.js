0ed31df22a1be95caa2f81b548b565ccb490051c
le-fonite <lefonite@gmail.com>
2024-01-19 23:45:07 +0200

    1

---
+++ HomeWork1_Hello-user/main.js
@@ -0,0 +1,15 @@
+// console.log('log2');
+// console.error('log');
+// const confirm = window.confirm('confirm')
+
+// let userName = prompt('Как ваше Имя?')
+// alert('Привет, '+userName+'! Как твои дела?')
+
+
+let userName = prompt('Как ваше Имя?');
+alert(`Привет, ${userName}! Как твои дела?`);
+
+
+
+
+

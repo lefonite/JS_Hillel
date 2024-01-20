e7a23b9b59288cd13a9608a51f8ea592705eb3c3
le-fonite <lefonite@gmail.com>
2024-01-20 22:34:01 +0200

    1

---
+++ HomeWork3_Maths/main.js
@@ -0,0 +1,38 @@
+
+// let 
+//     firstNumber = prompt('Введите первое число'),
+//     secondNumber = prompt('Введите второе число'),
+//     resultPlus = +firstNumber + +secondNumber,
+//     resultMinus = firstNumber - secondNumber,
+//     resultMultiply = firstNumber * secondNumber,
+//     resultDivision = firstNumber / secondNumber;
+
+//     alert('Если их сложить, получится:' + resultPlus );
+//     alert('Если их вычесть, получится:' + resultMinus );
+//     alert('Если их умножить, получится:' + resultMultiply );
+//     alert('Если их поделить, получится:' + resultDivision );
+
+let firstNumber = prompt("Ведите первое число:");
+let secondNumber = prompt("Введите второе число и получите рузультат:");
+
+let resultPlus = firstNumber + secondNumber;
+let resultMinus = firstNumber - secondNumber;
+let resultMultiply = firstNumber * secondNumber;
+let resultDivision = firstNumber / secondNumber;
+
+alert(`${firstNumber} + ${secondNumber} = ${resultPlus}
+\n${firstNumber} - ${secondNumber} = ${resultMinus}
+\n${firstNumber} * ${secondNumber} = ${resultMultiply}
+\n${firstNumber} / ${secondNumber} = ${resultDivision}`);
+
+
+
+
+
+
+
+
+
+
+
+
